4652751338e6b354b0914b40d0d3e69614dd6bb0
Your Name <shlittna@post.bgu.ac.il>
2025-01-07 14:08:41 +0200

    dd

--- src/test/java/CameraTest.java
+++ src/test/java/CameraTest.java
@@ -1,27 +1,20 @@
 
-import bgu.spl.mics.application.messages.DetectObjectsEvent;
-import bgu.spl.mics.application.objects.Camera;
-import bgu.spl.mics.application.objects.DetectedObject;
-import bgu.spl.mics.application.objects.LiDarDataBase;
-import bgu.spl.mics.application.objects.LiDarWorkerTracker;
-import bgu.spl.mics.application.objects.STATUS;
-import bgu.spl.mics.application.objects.TrackedObject;
+import java.nio.file.Paths;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import bgu.spl.mics.application.objects.Camera;
+import bgu.spl.mics.application.objects.DetectedObject;
 
 public class CameraTest {
     private Camera camera;
 
     @BeforeEach
     public void setUp() {
-        String cameraDataPath = Paths.get("example input/camera_data.json").toAbsolutePath().toString();
+        String cameraDataPath = Paths.get("example_input/camera_data.json").toAbsolutePath().toString();
         camera = new Camera("1", 10, "camera1",cameraDataPath);
     }
 
